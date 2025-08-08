4996140508d881097a6e817fcb00a9148031ede4
Finn <plesam466@gmail.com>
2025-08-08 22:13:46 +0800

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -2,7 +2,10 @@
 export interface UseSpeechSynthesisReturn {
   isSupported: boolean;
   isSpeaking: boolean;
+  isPaused: boolean;
   voices: SpeechSynthesisVoice[];
   speak: (options: { text: string; voice: SpeechSynthesisVoice | null; rate: number; pitch: number; volume: number; }) => void;
+  pause: () => void;
+  resume: () => void;
   cancel: () => void;
 }
